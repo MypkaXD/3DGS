67f61944675717e7510ef56cf672b0fa7cda2cf7
MypkaXD <nicklayschannel@yandex.ru>
2026-04-05 12:17:12 +0300

    start create gaussian rt

---
+++ Source/simple_ray_tracing/shaders/fragment_shader.frag
@@ -0,0 +1,12 @@
+#version 430 core
+
+uniform sampler2D compute_texture;
+
+in vec2 tex_coord;
+
+out vec4 FragColor;
+
+void main()
+{
+	FragColor = texture(compute_texture, tex_coord);
+}
--- Source/simple_ray_tracing/src/main.cpp
+++ Source/simple_ray_tracing/src/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <array>
 
 #define _USE_MATH_DEFINES
 #include <math.h>
@@ -57,23 +58,85 @@ int main()
 	compute_shader.load_shaders(std::make_pair("C:\\dev\\Gaussian_Splatting\\3DGS\\Source\\simple_ray_tracing\\shaders\\compute_shader.comp", GL_COMPUTE_SHADER));
 	compute_shader.link();
 
+	Shader shader;
+	shader.create();
+	shader.load_shaders(std::make_pair("C:\\dev\\Gaussian_Splatting\\3DGS\\Source\\simple_ray_tracing\\shaders\\vertex_shader.vert", GL_VERTEX_SHADER), std::make_pair("C:\\dev\\Gaussian_Splatting\\3DGS\\Source\\simple_ray_tracing\\shaders\\fragment_shader.frag", GL_FRAGMENT_SHADER));
+	shader.link();
+
 	unsigned int compute_texure;
 	glGenTextures(1, &compute_texure);
+	glActiveTexture(GL_TEXTURE0);
 	glBindTexture(GL_TEXTURE_2D, compute_texure);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-	glTexImage2D(GL_TEXTURE_2D, 0, GL_R32F, SCR_WIDTH, SCR_HEIGHT, 0, GL_RED, GL_FLOAT, NULL);
-	glBindImageTexture(0, compute_texure, 0, GL_FALSE, 0, GL_READ_WRITE, GL_R32F);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA32F, SCR_WIDTH, SCR_HEIGHT, 0, GL_RGBA, GL_FLOAT, NULL);
+	glBindImageTexture(0, compute_texure, 0, GL_FALSE, 0, GL_WRITE_ONLY, GL_RGBA32F);
 
+	float vertices[]  = {
+		-1.0f, -1.0f , 0.0f, 0.0f, 0.0f,
+		-1.0f,  1.0f , 0.0f, 0.0f, 1.0f,
+		 1.0f,  1.0f , 0.0f, 1.0f, 1.0f,
+		 1.0f, -1.0f , 0.0f, 1.0f, 0.0f,
+	};
+
+	unsigned int indices[] = {
+		0, 2, 1,
+		0, 3, 2
+	};
+
+	unsigned int VAO, VBO, EBO;
+
+	glGenVertexArrays(1, &VAO);
+
+	glBindVertexArray(VAO);
+
+	glGenBuffers(1, &VBO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	glGenBuffers(1, &EBO);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+
+	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
+	glEnableVertexAttribArray(1);
+
+	glBindVertexArray(0);
+
+	std::array<float, 3> light_source = { 10.0f, 10.0f, -5.0f };
+	std::array<float, 3> sphere_center = { 0.2f, 0.2f, 10.0f };
 
 	while (!glfwWindowShouldClose(window))
 	{
+
+		float current_time = static_cast<float>(glfwGetTime());
+
+		light_source[0] = 10.0f * cos(current_time);
+		light_source[1] = 10.0f * sin(current_time);
+
+		sphere_center[0] = 0.5f * cos(current_time);
+		sphere_center[1] = 0.5f * sin(current_time);
+
 		glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
 		compute_shader.use();
+		glUniform3f(glGetUniformLocation(compute_shader.get_id(), "light_source"), light_source[0], light_source[1], light_source[2]);
+		glUniform3f(glGetUniformLocation(compute_shader.get_id(), "sphere_center"), sphere_center[0], sphere_center[1], sphere_center[2]);
 		glDispatchCompute(SCR_WIDTH, SCR_HEIGHT, 1);
 		glMemoryBarrier(GL_ALL_BARRIER_BITS);
+
+		shader.use();
+		glBindVertexArray(VAO);
+		glActiveTexture(GL_TEXTURE0);
+		glBindTexture(GL_TEXTURE_2D, compute_texure);
+		glUniform1i(glGetUniformLocation(shader.get_id(), "compute_texture"), 0);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
 		ImGui_ImplOpenGL3_NewFrame();
 		ImGui_ImplGlfw_NewFrame();
